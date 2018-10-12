1fb7df870a8b62676274715fe7866e36f7ca4962
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-13 00:23:21 +0200

    Add cmd-line parsing

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello 🌍")
+	clusterSize := flag.Int("cluster-size", 3, "number of nodes in cluster")
+	id := flag.Int("id", 1, "node id")
+	flag.Parse()
+	fmt.Println("Cluster size:", *clusterSize)
+	fmt.Println("Node id:", *id)
 }
 
